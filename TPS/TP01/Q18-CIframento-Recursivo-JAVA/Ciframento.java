23e0407878cc7eb0de843af6988811abff090689
eduhatcc <eduardolwk@gmail.com>
2025-02-28 10:50:14 -0300

    Q18-Ciframento-Recursivo sucess

--- TPS/TP01/Q18-CIframento-Recursivo-JAVA/Ciframento.java
+++ TPS/TP01/Q18-CIframento-Recursivo-JAVA/Ciframento.java
@@ -2,37 +2,41 @@ import java.util.Scanner;
 
 public class Ciframento {
 
-	public static boolean verificaFim(String str, String chave) {
-		boolean fim = false;
+	public static boolean verificaFim(String str, String chave, int i) {
+		boolean fim = true;
 
-		if (str.length() == chave.length()) {
-			fim = true;
-
-			for (int i=0; i < chave.length(); i++) {
-				if (str.charAt(i) != chave.charAt(i)) {
-					fim = false;
-					i = chave.length();
+		if (i < chave.length()) {
+			if (str.length() == chave.length()) {
+					if (str.charAt(i) != chave.charAt(i)) {
+						fim = false;
+					}
+					else {
+						fim = verificaFim(str, chave, i+1);
+					}
 				}
+			else {
+				fim = false;
 			}
 		}
 
 		return fim;
 	}
 
-	public static String textbf(String str) {
+	public static String textbf(String str, int i) {
 		String cifra = "";
 
-		for (int i=0; i < str.length(); i++) {
-
+		if (i < str.length()) {
 			//verifica se a letra ou numero não é um caracter especial
 			if(str.charAt(i) < 127) { 
 
 				//concatenar os chars
 				cifra += (char) (str.charAt(i) + 3);
+				cifra += textbf(str, i+1);
 			} 
 			else {
 				//se for um caracter especial ele printa ele mesmo
 				cifra += str.charAt(i);
+				cifra += textbf(str, i+1);
 			}
 		}
 
@@ -45,8 +49,8 @@ public class Ciframento {
 		String str;
 		str = sc.nextLine();
 
-		while (!verificaFim(str, "FIM")) {
-			System.out.println(textbf(str));
+		while (!verificaFim(str, "FIM", 0)) {
+			System.out.println(textbf(str, 0));
 			str = sc.nextLine();	
 		}
 
