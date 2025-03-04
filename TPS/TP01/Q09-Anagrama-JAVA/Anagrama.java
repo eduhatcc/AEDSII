a1a3c814a69256137a28ccf6b865694d332f20fe
eduhatcc <eduardolwk@gmail.com>
2025-03-04 18:41:15 -0300

    Q09 sucess

--- TPS/TP01/Q09-Anagrama-JAVA/Anagrama.java
+++ TPS/TP01/Q09-Anagrama-JAVA/Anagrama.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Anagrama {
 
+	/*
+	 * FUNÇÃO VERIFICA FIM
+	 */
 	public static boolean verificaFim(String str, String chave) {
 		boolean fim = false;
 
@@ -19,36 +22,79 @@ public class Anagrama {
 		return fim;
 	}
 
-	public static boolean ehAnagrama(String str) {
-		boolean eh = true;
+	/*
+	 * FUNÇÃO VERIFICA FIM BASE
+	 */
+	public static boolean verificaFim(String str) {
+		return verificaFim(str, "FIM");
+	}
 
-		for (int i=0; i < str.length()/2; i++) {
-			for (int j=i*2; j < str.length(); j++) {
-				if (str.charAt(i) == str.charAt(j)) {
-					j = str.length();
+	/*
+	 * FUNÇÃO É ANAGRAMA
+	 */
+	public static boolean ehAnagrama(String str1, String str2) {
+		boolean eh = true;
+		
+		if (str1.length() != str2.length()) {
+			eh = false;
+		}
+		else {
+			int cont = 0;
+
+			for (int i=0; i < str1.length(); i++) { // laço da primeira string
+				for (int j=0; j < str2.length(); j++) { // laço da segunda string
+					
+					// if caso as duas letras sejam iguais contabilizar o contador
+					if (str1.charAt(i) == str2.charAt(j)) {
+						cont++;
+						j = str2.length();
+					}
+
+					// else if para caso a letra da primeira string seja maíuscula
+					else if (str1.charAt(i) >= 65 && str1.charAt(i) <= 90) {
+						char str1Min = (char) (str1.charAt(i) + 32); // converte a letra maíuscula para minúscula de acordo com a tabela ASCII
+						
+						if (str1Min == str2.charAt(j)) { 
+							cont++;
+							j = str2.length();
+						}
+					}
+
+					// else if para caso a letra da segunda string seja maíuscula
+					else if (str2.charAt(j) >= 65 && str2.charAt(j) <= 90) {
+						char str2Min = (char) (str2.charAt(j) + 32); // converte a letra maísculo para minúscula de acordo com a tabela ASCII
+
+						if (str1.charAt(i) == str2Min) {
+							cont++;
+							j = str2.length();
+						}
+					}
 				}
-				else if (j >= str.length() -1) {
-					eh = false;
-				}
+			}
+
+			// verificação se o contador é igual ao tamanho da string
+			if (cont != str1.length()) {
+				eh = false;
 			}
 		}
 
 		return eh;
 	}
 
+	/*
+	 * FUNÇÃO MAIN
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+		String str1 = sc.next();
+		String str2 = "";
 
-		while (!verificaFim(str, "FIM")) {
-			if (ehAnagrama(str)) {
-				System.out.println("SIM");
-			}
-			else {
-				System.out.println("NAO");
-			}
-			
-			str = sc.nextLine();
+		while (!verificaFim(str1)) {
+			sc.next();
+			str2 = sc.next();
+
+			System.out.println((ehAnagrama(str1, str2)) ? "SIM" : "NÃO");
+			str1 = sc.next();
 		}
 
 		sc.close();
