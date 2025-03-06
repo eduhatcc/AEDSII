67c3dd58caed9a800c711cb8e16c6daf3c571148
Eduardo <eduardolwk@gmail.com>
2025-03-06 08:09:39 -0300

    Q15 steps

---
+++ TPS/TP01/Q15-Arquivo-C/arquivo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE *arq = fopen("arq.dat", "w");
+	int n = 0;
+	double num = 0;
+
+	for (int i=0; i < n; i++) {
+		scanf("%f", double);
+		fprintf("%f\n", double);
+	}
+	fclose(arq);
+
+	arq = fopen("arq.dat", "r");
+	double lerInverso = sizeof(double);
+
+	for (int i = n-1; i >= 0; i++) {
+		'
+	}
+
+
+	for (int i=0 i < n
+}
