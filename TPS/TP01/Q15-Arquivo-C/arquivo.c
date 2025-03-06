bf60950c9f7645787a5ce873c92cae00df911fc0
eduhatcc <eduardolwk@gmail.com>
2025-03-06 17:46:16 -0300

    Q15 sucess

--- TPS/TP01/Q15-Arquivo-C/arquivo.c
+++ TPS/TP01/Q15-Arquivo-C/arquivo.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	FILE *arq = fopen("arq.dat", "w");
-	int n = 0;
-	double num = 0;
+int main () {
+    int n;
+    char line[256];
+    
+    scanf("%d", &n);
+    
+    float in[n];
+    float out[n];
+    
+    FILE *arq = fopen("arq.dat", "w");
+    
+    for (int i = 0; i < n; i++) {
+        scanf(" %f", &in[i]);
 
-	for (int i=0; i < n; i++) {
-		scanf("%f", double);
-		fprintf("%f\n", double);
-	}
-	fclose(arq);
-
-	arq = fopen("arq.dat", "r");
-	double lerInverso = sizeof(double);
-
-	for (int i = n-1; i >= 0; i++) {
-		'
-	}
-
-
-	for (int i=0 i < n
+        if (i == n-1) {
+            fprintf(arq, "%g", in[i]);
+        }
+	else {
+            fprintf(arq, "%g\n", in[i]);
+        }
+    }
+    fclose(arq);
+    
+    arq = fopen("arq.dat", "r");
+    
+    for (int i = 0; i < n; i++) {
+        fgets(line, sizeof(line), arq);
+        sscanf(line, "%f", &out[i]);
+    }
+    fclose(arq);
+    
+    for (int i = n-1; i >= 0; i--) {
+        if ((int)out[i] == out[i]) {
+            printf("%.0f\n", out[i]);
+        } else {
+            printf("%g\n", out[i]);
+        }
+    }
+    
+    return 0;
 }
