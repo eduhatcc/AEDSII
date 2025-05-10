54a5fe810b4b32e796404c78fa4871d698d83720
eduhatcc <eduardolwk@gmail.com>
2025-05-10 17:22:53 -0300

    sucess 1025 Onde está o mármore

--- BEECROWD/1025-Onde-Esta-o-Marmore?/Main.java
+++ BEECROWD/1025-Onde-Esta-o-Marmore?/Main.java
@@ -20,19 +20,19 @@ public class Main {
 		}
 	}
 
-	public static int pesquisaBinaria(int[] array, int esq, int dir, int Q) {
+	public static int pesquisaBinaria(int[] array, int esq, int dir, int Q, int src, int i) {
 		int meio = (esq+dir) /2,
 		    resp = 0;
 
-		if (esq <= dir) {
-			if (array[meio] == Q) {
+		if (esq <= dir && i < Q) {
+			if (array[meio] == src) {
 				resp = meio+1;
 			}
-			else if (array[meio] < Q) {
-				resp = pesquisaBinaria(array, meio+1, dir, Q);
+			else if (array[meio] < src) {
+				resp = pesquisaBinaria(array, meio+1, dir, Q, src, i++);
 			}
 			else {
-				resp = pesquisaBinaria(array, esq, meio-1, Q);
+				resp = pesquisaBinaria(array, esq, meio-1, Q, src, i++);
 			}
 		}
 
@@ -42,31 +42,44 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt(),
-		    Q = sc.nextInt(),
-		    i = 0,
+		int N = sc.nextInt();
+		int Q = sc.nextInt();
+		int i = 0,
 		    caso = 1,
 		    resp = 0;
 		int[] array = new int[65];
+		int[] src = new int[65];
 		
-		while (n > 0 && Q > 0) {
-			while (i < n) {
+		while (N > 0 && Q > 0) {
+			while (i < N) {
 				array[i] = sc.nextInt();
 				i++;
 			}
+
 			ordenaArray(array, N);
 			i = 0;
 
 			while (i < Q) {
-				resp = pesquisaBinaria(array, N, Q, caso);
+				src[i] = sc.nextInt();
+				i++;
+			}
+
+			i = 0;
+			System.out.println("CASE# " + caso + ":");
+			while (i < Q) {
+				resp = pesquisaBinaria(array, 0, N, Q, src[i], 0);
 
 				if (resp == 0) {
-					System.out.println(Q + " not found");
+					System.out.println(src[i] + " not found");
 				}
 				else {
-					System.out.println(Q + " found at " + resp);
+					System.out.println(src[i] + " found at " + resp);
 				}
+				i++;
 			}
+			caso++; i = 0;
+			N = sc.nextInt();
+			Q = sc.nextInt();
 		}
 
 		sc.close();
