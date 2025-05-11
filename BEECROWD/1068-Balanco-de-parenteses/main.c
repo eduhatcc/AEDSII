8bf48d38bdbb54c607489aeb08ff566a6ee92cce
eduhatcc <eduardolwk@gmail.com>
2025-05-11 13:03:42 -0300

    sucess 1068 Balanço de Parênteses - C

--- BEECROWD/1068-Balanco-de-parenteses/main.c
+++ BEECROWD/1068-Balanco-de-parenteses/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool balanco(char str[]) {
+	int esq = 0,
+	    dir = 0;
+	bool valido = false;
+
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '(') esq++;
+		else if (str[i] == ')') dir++;
+	}
+
+	if (esq == dir) valido = true;
+
+	return valido;
+}
 
 int main() {
-	/*
-	 * FAZER TODA A IMPLEMENTAÇÃO
-	 */
+	char str[100];
+
+	while (fgets(str, sizeof(str), stdin)) {
+		str[strlen(str) -1] = '\0';
+		
+		if (balanco(str)) printf("correct\n");
+		else printf("incorrect\n");
+	}
 
 	return 0;
 }
