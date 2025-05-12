1295e7de4971a97909e0ea64d772680be127a1d3
Eduardo <eduardolwk@gmail.com>
2025-05-12 10:24:07 -0300

    create: Construtor, Inserir, InserirPai, Pesquisar

---
+++ NOTES/U4-Estruturas-de-Dados-Flexiveis/JAVA/ArvoreBinaria/Arvore.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Arvore {
+	No raiz;
+
+	public Arvore() {
+		raiz = null;
+	}
+
+	public void inserir(int x) {
+		raiz = inserir(x, raiz);
+	}
+
+	/* 
+	 * MÉTODO ABAIXO INSERIDO NA CLASSE NÓ
+	 *
+	public No inserir(int x, No i) throws new Exception {
+		if (i == null) i = new No(x);
+		else if (x < i.elemento) i.esq = inserir(x, i.esq);
+		else if (x > i.elemento) i.dir = inserir(x, i.dir);
+		else throw new Exception("Erro!");
+
+		return i;
+	}
+	*/
+
+	public void inserirPai(int x) throws new Exception {
+		if (i == null) i = new No(x);
+		else if (x < raiz.elemento) inserirPai(x, raiz.esq, raiz);
+		else if (x > raiz.elemento) inserirPai(x, raiz.dir, raiz);
+		else throw new Exception("Erro!");
+	}
+
+	/*
+	 * MÉTODO ABAIXO É PARA SER INSERIDO NA CLASSE NÓ
+	 */
+	public void inserirPai(int x, No i, No pai) throws new Exception {
+		if (i = null) {
+			if (x < pai.elemento) pai.esq = new No(x);
+			else pai.dir = new No(x);
+		}
+		else if (x < i.elemento) i.esq = inserirPai(x, i.esq, i);
+		else if (x > i.elemento) i.dir = inserirPai(x, i.dir, i);
+		else throw new Exception("Erro!");
+	}
+
+	public boolean pesquisar(int x) {
+		return pesquisar(x, raiz);
+	}
+
+	/*
+	 * MÉTODO ABAIXO É PARA SER INSERIDO NA CLASSE NÓ
+	 */
+	public boolean pesquisar(int x, No i) {
+		boolean find = false;
+
+		if (i != null) {
+			if (x == i.elemento) find = true;
+			else if (x < i.elemento) find = pesquisar(x, i.esq);
+			else if (x > i.elemento) find = pesquisar(x, i.dir);
+		}
+
+		return find;
+	}
+
+	public void caminharCentral() {
+		caminharCentral(raiz);
+	}
+
+	public void caminharCentral(No i) {
+
+	}
+
+	public void caminharPre() {
+
+	}
+
+	public void caminharPos() {
+
+	}
+
+	public void remover(int x) {
+
+	}
+}
