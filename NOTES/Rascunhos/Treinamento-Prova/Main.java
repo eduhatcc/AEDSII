8ed9f62ddffc1a9f5219d3588a48bec530b80324
Eduardo <eduardolwk@gmail.com>
2025-05-12 16:57:52 -0300

    steps Treinamento para prova

---
+++ NOTES/Rascunhos/Treinamento-Prova/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class Pessoa {
+	private int id;
+	private String nome;
+	private int idade;
+	private String[] cor;
+	private String palavra;
+
+	public Pessoa () {}
+
+	public Pessoa(int id, String nome, int idade, String[] cor, String palavra) {
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		for (int i = 0; i < cor.length; i++) {
+			this.cor[i] = cor[i];
+		}
+		this.palavra = palavra;
+	}
+
+	public void mostrar() {
+
+	}
+}
+
+public class Main {
+	String[] splitDoEduardo(String linha){
+		boolean dentroAspas = false;
+		String[] resp = new String[10];
+		String palavra = "ola mundo";
+		palavra = "";
+			
+	}
+
+
+	public class void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String line = sc.nextLine();
+
+		String[] campos = new String[5];
+		int index = 0;
+		boolean aspas = false;
+
+		int id;
+		String nome;
+		int idade;
+		String[] cor = new String[500];
+		String palavra;
+
+		for (int i = 0; i < n; i++) {
+			id = sc.nextInt();
+			sc.next();
+			nome = sc.next();
+		}
+	}
+}
