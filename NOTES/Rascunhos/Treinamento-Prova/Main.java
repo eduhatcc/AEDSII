7c31eebc6a009228807fcb9e56e0ed060a296ec9
eduhatcc <eduardolwk@gmail.com>
2025-05-12 22:49:40 -0300

    sucess sem ordenação

--- NOTES/Rascunhos/Treinamento-Prova/Main.java
+++ NOTES/Rascunhos/Treinamento-Prova/Main.java
@@ -1,58 +1,106 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 class Pessoa {
 	private int id;
 	private String nome;
 	private int idade;
-	private String[] cor;
+	private String[] cores;
 	private String palavra;
 
 	public Pessoa () {}
 
-	public Pessoa(int id, String nome, int idade, String[] cor, String palavra) {
+	public Pessoa(int id, String nome, int idade, String[] cores, String palavra) {
 		this.id = id;
 		this.nome = nome;
 		this.idade = idade;
-		for (int i = 0; i < cor.length; i++) {
-			this.cor[i] = cor[i];
-		}
+		this.cores = cores.clone();
 		this.palavra = palavra;
 	}
 
-	public void mostrar() {
+	public int getId() {
+		return this.id;
+	}
 
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public String[] getCores() {
+		return this.cores;
+	}
+
+	public String getPalavra() {
+		return this.palavra;
+	}
+
+	public void imprimir() {
+		System.out.printf("%d %s %d %s %s%n", id, nome, idade, Arrays.toString(cores), palavra);
 	}
 }
 
 public class Main {
-	String[] splitDoEduardo(String linha){
-		boolean dentroAspas = false;
-		String[] resp = new String[10];
-		String palavra = "ola mundo";
-		palavra = "";
-			
+	public static void swap(Pessoa[] p, int i, int j) {
+		Pessoa tmp = p[i];
+		p[i] = p[j];
+		p[j] = tmp; 
 	}
 
+	public static void selectionSort(Pessoa[] p, int index) {
+		int menor = 0;
+		for (int i = 0; i < index; i++) {
+			menor = i;
+			for (int j = i+1; j < index; j++) {
+				if (p[j].getId() < p[menor].getId()) menor = j;
+			}
+			swap(p, i, menor);
+		}
+	}
 
-	public class void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String line = sc.nextLine();
-
-		String[] campos = new String[5];
+		Pessoa[] p = new Pessoa[50];
 		int index = 0;
-		boolean aspas = false;
 
-		int id;
-		String nome;
-		int idade;
-		String[] cor = new String[500];
-		String palavra;
+		String line = sc.nextLine();
+		while (!line.equals("FIM")) {
+			String[] campos = new String[5];
+			int cont = 0;
+			boolean aspas = false;
+			StringBuilder str = new StringBuilder();
 
-		for (int i = 0; i < n; i++) {
-			id = sc.nextInt();
-			sc.next();
-			nome = sc.next();
+			for (int i = 0; i < line.length(); i++) {
+				char c = line.charAt(i);
+
+				if (c == '"') aspas = !aspas;
+				else if (c == ',' && !aspas) {
+					campos[cont++] = str.toString();
+					str.setLength(0);
+				}
+				else str.append(c);
+			}
+
+			campos[cont] = str.toString();
+
+			int id = Integer.parseInt(campos[0]);
+			String nome = campos[1];
+			int idade = Integer.parseInt(campos[2]);
+			String[] cores = campos[3].split(",");
+			String palavra = campos[4];
+
+			p[index++] = new Pessoa(id, nome, idade, cores, palavra);
+
+			line = sc.nextLine();
+		}
+
+		selectionSort(p, index);
+
+		for (int i = 0; i < index; i++) {
+			p[i].imprimir();		
 		}
 	}
 }
