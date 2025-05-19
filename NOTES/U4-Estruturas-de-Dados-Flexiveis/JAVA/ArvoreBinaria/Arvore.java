084152668165dc1f93f98ed5099016d36092373e
Eduardo <eduardolwk@gmail.com>
2025-05-19 10:25:37 -0300

    Implementado métodos 'Caminhar Pré-Ordem', 'Caminhar Pós-Ordem' e 'Caminhar Central'

--- NOTES/U4-Estruturas-de-Dados-Flexiveis/JAVA/ArvoreBinaria/Arvore.java
+++ NOTES/U4-Estruturas-de-Dados-Flexiveis/JAVA/ArvoreBinaria/Arvore.java
@@ -63,20 +63,61 @@ public class Arvore {
 		return find;
 	}
 
+	/* Caminhar Central Recursivo
+	 * ( Imprime em ordem decrescente )
+	 *
+	 */
+	public caminharCentral(No i) {
+		if (i != null) {
+			caminharCentral(i.esq);
+			System.out.print(i.elemento + " ");
+			caminharCentral(i.dir);
+		}
+	}
+
+	/* 
+	 * Caminhar Central Base
+	 */
 	public void caminharCentral() {
 		caminharCentral(raiz);
 	}
 
-	public void caminharCentral(No i) {
-
+	/*
+	 * Caminhar Pré-Ordem Recursivo
+	 * ( Imprime primeiros os pais para depois imprimir os filhos )
+	 */
+	public void caminharPre(No i) {
+		if (i != null) {
+			System.out.print(i.elemento + "");
+			caminharPre(i.esq);
+			caminharPre(i.dir);
+		}
 	}
 
+	/*
+	 * Caminhar Pré-Ordem Base
+	 */
 	public void caminharPre() {
-
+		caminharPre(raiz);
 	}
 
-	public void caminharPos() {
+	/*
+	 * Caminhar Pós-Ordem Recursivo
+	 * ( Imprime primeiro os filhos para depois imprimir os pais )
+	 */
+	public void caminharPos(No i) {
+		if (i != null) {
+			caminharPos(i.esq);
+			caminharPos(i.dir);
+			System.out.print(i.elemento + "");
+		}
+	}
 
+	/* 
+	 * Caminhar Pós-Ordem Base
+	 */
+	public void caminharPos() {
+		caminharPos(raiz;);
 	}
 
 	public void remover(int x) {
