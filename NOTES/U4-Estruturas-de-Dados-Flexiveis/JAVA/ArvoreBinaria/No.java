8dd9ad6555e7942d157ba3a21b038e71f988240d
Eduardo <eduardolwk@gmail.com>
2025-05-19 10:28:19 -0300

    Implementado métodos da classe Nó: 'Caminhar Pré-Ordem', 'Caminhar Pós-Ordem' e 'Caminhar Central'

--- NOTES/U4-Estruturas-de-Dados-Flexiveis/JAVA/ArvoreBinaria/No.java
+++ NOTES/U4-Estruturas-de-Dados-Flexiveis/JAVA/ArvoreBinaria/No.java
@@ -20,4 +20,60 @@ public class No {
 		return i;
 	} 
 
+		/* Caminhar Central Recursivo
+	 * ( Imprime em ordem decrescente )
+	 *
+	 */
+	public caminharCentral(No i) {
+		if (i != null) {
+			caminharCentral(i.esq);
+			System.out.print(i.elemento + " ");
+			caminharCentral(i.dir);
+		}
+	}
+
+	/* 
+	 * Caminhar Central Base
+	 */
+	public void caminharCentral() {
+		caminharCentral(raiz);
+	}
+
+	/*
+	 * Caminhar Pré-Ordem Recursivo
+	 * ( Imprime primeiros os pais para depois imprimir os filhos )
+	 */
+	public void caminharPre(No i) {
+		if (i != null) {
+			System.out.print(i.elemento + "");
+			caminharPre(i.esq);
+			caminharPre(i.dir);
+		}
+	}
+
+	/*
+	 * Caminhar Pré-Ordem Base
+	 */
+	public void caminharPre() {
+		caminharPre(raiz);
+	}
+
+	/*
+	 * Caminhar Pós-Ordem Recursivo
+	 * ( Imprime primeiro os filhos para depois imprimir os pais )
+	 */
+	public void caminharPos(No i) {
+		if (i != null) {
+			caminharPos(i.esq);
+			caminharPos(i.dir);
+			System.out.print(i.elemento + "");
+		}
+	}
+
+	/* 
+	 * Caminhar Pós-Ordem Base
+	 */
+	public void caminharPos() {
+		caminharPos(raiz;);
+	}
 }
