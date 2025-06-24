622d08d93baebaaacbd297ebe0001dc4490558b5
eduhatcc <eduardolwk@gmail.com>
2025-06-23 22:36:54 -0300

    sucess 2872 Protocolo TCP/IP JAVA

--- BEECROWD/2872-ProtocoloTCP-IP/Main.java
+++ BEECROWD/2872-ProtocoloTCP-IP/Main.java
@@ -4,24 +4,37 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String word = new String();
+		String word[] = new String[100];
 		int i = 0;
 		word[i] = sc.nextLine();
 
-		while (word[i] == 1) {
-
-			while (word[i] != 0) {
-				word[i] = sc.nextLine();
+		while (!word[i].equals("0")) {
+			while (!word[i].equals("0")) {
 				i++;
+				word[i] = sc.nextLine();
+				
 			}
-			i--;
 
-			for (int j = 0; j < i; j++) {
-				int maior = j;
+			for (int j = 1; j < i; j++) {
+				int menor = j;
 				for (int k = j+1; k < i; k++) {
-					if (word[j].comparetoIgnoreCase(word[maior]) > 0)
+					if (word[k].compareToIgnoreCase(word[menor]) < 0) {
+						menor = k;
+					}
 				}
+
+				String tmp = word[j];
+				word[j] = word[menor];
+				word[menor] = tmp;
 			}
+
+			for (int j = 1; j < i; j++) {
+				System.out.println(word[j]);
+			}
+			System.out.println();
+
+			i = 0;
+			word[i] = sc.nextLine();
 		}
 
 		sc.close();
